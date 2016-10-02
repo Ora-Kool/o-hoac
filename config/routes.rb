54cdce184dc03cbb61d48bcc46b2f5528fdc8aee
Ora-Kool <etingemabian@gmail.com>
2016-10-02 01:27:32 +0100

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'page#home'
+  get '/about', to: 'page#about'
+  get '/terms', to: 'page#terms'
+  get '/faqs', to: 'page#faqs'
+
+  get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
